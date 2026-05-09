ffbf562d7c0be090c6d2e00328548035e163bda8
KAPIL BAHADUR CHHETRI <kapilkc172@gmail.com>
2026-05-09 22:37:36 +0545

    first commit

---
+++ day-1/naming-variable.py
@@ -0,0 +1,10 @@
+# 1. make a readable variable name.
+# 2. number aren't used as first while creating variable
+
+
+name1 = "Messi"
+name2 = "Ronaldo"
+name3 = "Neymar"
+
+length = len(name1),  len(name2), len(name3)
+print(length)
